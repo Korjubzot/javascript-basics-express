b03c19ce06bb0418aa0d63f0abb17bc4c737955e
Korjubzot <korjubzot@gmail.com>
2023-06-14 15:49:06 +0100

    Made it to final strings test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const {
+  sayHello,
+  uppercase,
+  lowercase,
+  countCharacters,
+  firstCharacter,
+  firstCharacters,
+} = require('./lib/strings');
 
 const app = express();
 
-app.get('/strings/hello/world', (req, res) => {
-  res.status(200).json({ result: 'Hello, world!' });
+app.get('/strings/hello/:string', (req, res) => {
+  res.json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/count/:string', (req, res) => {
+  res.json({ result: countCharacters(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.json({ result: firstCharacter(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  const { string } = req.params;
+  const { length } = req.query;
+
+  res.json({ result: firstCharacters(string, length) });
 });
 
 module.exports = app;
