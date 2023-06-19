a5841ea5cfdad7a6690cd8130d63385e789c00a9
Korjubzot <korjubzot@gmail.com>
2023-06-19 21:15:07 +0100

    Passed multiplier tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable radix */
 const express = require('express');
+
 const {
   sayHello,
   uppercase,
@@ -24,6 +25,7 @@ const {
 } = require('./lib/numbers');
 
 const app = express();
+app.use(express.json());
 
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
@@ -64,8 +66,8 @@ app.get('/numbers/add/:a/and/:b', (req, res) => {
 });
 
 app.get('/numbers/subtract/:a/from/:b', (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
+  const a = parseFloat(req.params.a);
+  const b = parseFloat(req.params.b);
 
   if (Number.isNaN(a) || Number.isNaN(b)) {
     res.status(400).send({ error: 'Parameters must be valid numbers.' });
@@ -75,8 +77,22 @@ app.get('/numbers/subtract/:a/from/:b', (req, res) => {
 });
 
 app.post('/numbers/multiply', (req, res) => {
-  const { a, b } = req.body.a;
+  const { a, b } = req.body;
+
+  if (!a || !b) {
+    res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  }
+  if (isNaN(a) || isNaN(b)) {
+    res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+
   const result = multiply(a, b);
+  res.status(200).send({ result });
+});
+
+app.post('/numbers/divide', (req, res) => {
+  const { a, b } = req.body;
+  const result = divide(a, b);
 
   res.status(200).json({ result });
 });
